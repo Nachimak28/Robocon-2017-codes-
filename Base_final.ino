bc351f34eea91f244cbd3090b83e8779ce312ce9
#nachimak <nachimak28@gmail.com>
2017-05-30 16:33:22 +0530

    Add files via upload

---
+++ Base_final.ino
@@ -0,0 +1,421 @@
+/*SFIT ROBOCON TEAM
+ *CONFIDENTIAL
+  23/02/2017
+ *Wall following locomotion code BETA
+ *Straight Outta Programmer's Desk
+ *
+*/
+
+//BETA
+
+#include <Cytron_PS2Shield.h>
+#include <Wire.h>
+
+Cytron_PS2Shield ps2(10,11); //Rx, Tx
+
+
+#define MAX_ERROR 40
+#define MIN_ERROR -40
+#define pwm1 7
+#define pwm2 6
+#define pwm3 8
+#define pwm4 9
+#define dir1 28
+#define dir2 30
+#define dir3 24
+#define dir4 26
+
+#define TRIG1 43 // ultra 1
+#define ECHO1 45
+#define TRIG2 47 // ultra 2 
+#define ECHO2 49
+#define RELAY1 35
+#define RELAY2 37
+#define manualSpeed 100 // speed for manual locomotion // initially it was 40
+#define turnSpeed 60
+
+void setSpeed(float);
+void readPS2(void);
+void sendData(char);
+int calculateError(void);
+void accelerate(void);
+void manualLoco(void);
+void shoot(int);
+int setPoint = 65;
+int currentSpeed = 0;
+
+void setup(){
+  Wire.begin();
+  ps2.begin(9600);
+  Serial.begin(9600);
+  pinMode(pwm1, OUTPUT);
+  pinMode(pwm2, OUTPUT);
+  pinMode(pwm3, OUTPUT);
+  pinMode(pwm4, OUTPUT);
+  
+  pinMode(dir1, OUTPUT);
+  pinMode(dir2, OUTPUT);
+  pinMode(dir3, OUTPUT);
+  pinMode(dir4, OUTPUT);
+//  pinMode(AN, INPUT);
+//  digitalWrite(UEN, LOW);
+  pinMode(TRIG1, OUTPUT);
+  pinMode(TRIG2, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(ECHO2, INPUT);
+  
+  analogWrite(pwm1, 0);
+  analogWrite(pwm2, 0);
+  analogWrite(pwm3, 0);
+  analogWrite(pwm4, 0);
+//pins for relay
+//  pinMode(32,OUTPUT);
+//  digitalWrite(32, HIGH);
+  pinMode(RELAY1,OUTPUT);
+  pinMode(RELAY2,OUTPUT); 
+//  pinMode(SLAVE_RESET, OUTPUT);
+//  digitalWrite(SLAVE_RESET, LOW); 
+ }
+
+int integral = 0; 
+int proportional = 0;
+int derivative = 0;
+float kp = 1.8;
+float kd = 1;
+//float ki = 0;
+long int prevMillis = 0;
+int lastError = 0;
+int foo = 0;
+int error = 0;
+int averageSpeed = 150; // initially 150
+bool change = false;
+int bar = 0; // joystick right (manual loco)
+int relayPin = RELAY2; //R1 default relayPin
+//bool toggle = false;
+
+void loop() {
+
+  readPS2();
+   //Serial.println(foo); 
+  if(foo){ 
+  //  averageSpeed = foo + 30;
+  //Line following section
+ /*         
+      error = map(analogRead(AN),0,910,-20,20);
+      if(error > 20 )
+        error = lastError;
+*/
+  //Wall following section
+    error = calculateError();
+  //  error = 0;
+    if(error > MAX_ERROR)
+      error = MAX_ERROR;
+    else if(error < MIN_ERROR)
+      error = MIN_ERROR;
+ //   if(error > -8 && error < 8)
+ //     integral = 0;
+    //Serial.println(error);
+    proportional = error;
+    derivative = (error - lastError);
+  //  integral = integral + error;
+    float turn = proportional*kp + derivative*kd;
+//      Serial.print("turn   = ");
+//      Serial.println(turn);   
+//      prevMillis = millis();
+      setSpeed(turn);  
+    } 
+//    Serial.print(" \t");
+//    Serial.println(error); 
+}
+
+
+void setSpeed(float turn){
+      int speed1 = averageSpeed + turn;
+      int speed2 = averageSpeed - turn;
+    
+      if(speed1 > 255) 
+        speed1 = 255;
+      if(speed2 > 255)
+        speed2 = 255;
+      if(speed1 < 0)
+        speed1 = 0;
+      if(speed2 < 0)
+        speed2 = 0;
+        
+      analogWrite(pwm1, speed1);
+      analogWrite(pwm2, speed1);
+      analogWrite(pwm3, speed2);
+      analogWrite(pwm4, speed2);
+//      Serial.print(speed1);
+//      Serial.print("\t");
+//      Serial.println(speed2);      
+//      Serial.print(" \tError = ");
+//      Serial.println(error);
+//      Serial.print("Turn = ");
+//      Serial.println(turn);
+      lastError = error; 
+}
+
+void readPS2(){
+ // Serial.println("zxc");
+    if(!ps2.readButton(PS2_TRIANGLE)){
+        sendData('U');
+        while(!ps2.readButton(PS2_TRIANGLE));
+      
+      }
+    else if(!ps2.readButton(PS2_CROSS)){
+        sendData('D');
+        while(!ps2.readButton(PS2_CROSS));
+
+      }
+    else if(!ps2.readButton(PS2_UP)){
+        sendData('+');
+        while(!ps2.readButton(PS2_UP));
+
+      }
+    else if(!ps2.readButton(PS2_DOWN)){
+        sendData('-');
+        while(!ps2.readButton(PS2_DOWN));
+       
+      }
+    else if(!ps2.readButton(PS2_LEFT_1)){
+        sendData('L');
+        while(!ps2.readButton(PS2_LEFT_1));
+       // relayPin = 37;
+      }
+    else if(!ps2.readButton(PS2_LEFT_2)){
+        sendData('Z');
+        while(!ps2.readButton(PS2_LEFT_2));
+      //  relayPin = 35;
+      }
+     else if(!ps2.readButton(PS2_RIGHT_1)){
+       // shoot(RELAY1);
+        digitalWrite(RELAY1, HIGH);
+        delay(100);
+        digitalWrite(RELAY1, LOW);
+       // relayPin = 37;
+      }
+    else if(!ps2.readButton(PS2_CIRCLE) || !ps2.readButton(PS2_SQUARE)){
+     /*   sendData('S');
+        while(!ps2.readButton(PS2_CIRCLE) || !ps2.readButton(PS2_SQUARE));
+        delay(500);
+        sendData('X'); */
+        shoot(relayPin);
+      //  Serial.println("back circle");
+      }
+ /*   else if(!ps2.readButton(PS2_LEFT_2) || !ps2.readButton(PS2_RIGHT_2)){
+//       /* sendData('S');
+//        
+//        delay(100);
+//        sendData('X');
+//         */
+//        relayPin = 37;
+//        shoot(relayPin);
+//        while(!ps2.readButton(PS2_SQUARE));
+//        Serial.println("back square");
+//          digitalWrite(SLAVE_RESET, HIGH);
+//          delay(500);
+//          digitalWrite(SLAVE_RESET, LOW);
+//      } 
+// ULTRA dependent Joystick
+    else if(ps2.readButton(PS2_JOYSTICK_LEFT_UP)){
+      digitalWrite(dir1, HIGH);
+      digitalWrite(dir2, HIGH);
+      digitalWrite(dir3, HIGH);
+      digitalWrite(dir4, HIGH);
+      foo = ps2.readButton(PS2_JOYSTICK_LEFT_UP);
+      while(currentSpeed <= averageSpeed && foo){
+       // Serial.println(foo);
+        accelerate();
+        foo = ps2.readButton(PS2_JOYSTICK_LEFT_UP);
+        }
+        if(!foo){
+        freeze();
+        }  
+     }
+    else if(ps2.readButton(PS2_JOYSTICK_LEFT_DOWN)){
+      digitalWrite(dir1, LOW);
+      digitalWrite(dir2, LOW);
+      digitalWrite(dir3, LOW);
+      digitalWrite(dir4, LOW);
+      foo = ps2.readButton(PS2_JOYSTICK_LEFT_DOWN);
+      while(currentSpeed <= averageSpeed && foo){
+       // Serial.println(foo);
+        accelerate();
+        foo = ps2.readButton(PS2_JOYSTICK_LEFT_DOWN);
+      }
+      if(!foo){
+          freeze();
+        } 
+    }
+// Manual override joystick
+    else if(ps2.readButton(PS2_JOYSTICK_RIGHT_UP)){
+      digitalWrite(dir1, HIGH);
+      digitalWrite(dir2, HIGH);
+      digitalWrite(dir3, HIGH);
+      digitalWrite(dir4, HIGH);
+      bar = ps2.readButton(PS2_JOYSTICK_RIGHT_UP);
+      while(currentSpeed <= manualSpeed && bar){
+       // Serial.println(foo);
+        accelerate();
+        bar = ps2.readButton(PS2_JOYSTICK_RIGHT_UP);
+        }
+        if(!bar){
+        freeze();
+        } 
+    //  Serial.println(bar);
+       }
+
+    else if(ps2.readButton(PS2_JOYSTICK_RIGHT_DOWN)){
+      digitalWrite(dir1, LOW);
+      digitalWrite(dir2, LOW);
+      digitalWrite(dir3, LOW);
+      digitalWrite(dir4, LOW);
+      bar = ps2.readButton(PS2_JOYSTICK_RIGHT_DOWN);
+      while(currentSpeed <= manualSpeed && bar){
+       // Serial.println(foo);
+        accelerate();
+        bar = ps2.readButton(PS2_JOYSTICK_RIGHT_DOWN);
+        }
+        if(!bar){
+        freeze();
+        }
+   //   Serial.println(bar);
+      }
+    else if(ps2.readButton(PS2_JOYSTICK_RIGHT_LEFT)){
+      digitalWrite(dir1, HIGH);
+      digitalWrite(dir2, HIGH);
+      digitalWrite(dir3, LOW);
+      digitalWrite(dir4, LOW);
+      bar = ps2.readButton(PS2_JOYSTICK_RIGHT_LEFT);
+//      while(currentSpeed <= manualSpeed && bar){
+//       // Serial.println(foo);
+//        accelerate();
+//        bar = ps2.readButton(PS2_JOYSTICK_RIGHT_LEFT);
+//        }
+//        if(!bar){
+//        freeze();
+//        }
+      while(bar){
+          manualLoco();
+          bar = ps2.readButton(PS2_JOYSTICK_RIGHT_LEFT);
+        }
+      analogWrite(pwm1, 0);
+      analogWrite(pwm2, 0);
+      analogWrite(pwm3, 0);
+      analogWrite(pwm4, 0); 
+    //  Serial.println(bar);
+     }
+    else if(ps2.readButton(PS2_JOYSTICK_RIGHT_RIGHT)){
+      digitalWrite(dir1, LOW);
+      digitalWrite(dir2, LOW);
+      digitalWrite(dir3, HIGH);
+      digitalWrite(dir4, HIGH);
+      bar = ps2.readButton(PS2_JOYSTICK_RIGHT_RIGHT);
+      while(bar){
+          manualLoco();
+          bar = ps2.readButton(PS2_JOYSTICK_RIGHT_RIGHT);
+        }      
+      analogWrite(pwm1, 0);
+      analogWrite(pwm2, 0);
+      analogWrite(pwm3, 0);
+      analogWrite(pwm4, 0);      
+   //   Serial.println(bar);
+      }
+   else{
+      foo = 0;
+      bar = 0;
+      freeze();
+   }
+}
+
+void sendData(char bar){
+      Wire.beginTransmission(8);
+      Wire.write(bar);      
+      Wire.endTransmission();
+     // Serial.println(bar);
+}
+
+int calculateError(){
+     //int a = analogRead(SENSOR_FRONT);
+     //int b = analogRead(SENSOR_BACK);
+     digitalWrite(TRIG1, LOW);
+     delayMicroseconds(2);
+     digitalWrite(TRIG1, HIGH);
+     delayMicroseconds(5);
+     digitalWrite(TRIG1, LOW);
+
+     int a = pulseIn(ECHO1, HIGH)/29/2;
+   //  int a=60;
+     digitalWrite(TRIG2, LOW);
+     delayMicroseconds(2);
+     digitalWrite(TRIG2, HIGH);
+     delayMicroseconds(5);
+     digitalWrite(TRIG2, LOW);
+
+     int b = pulseIn(ECHO2, HIGH)/29/2;
+     Serial.print(a);
+     Serial.print(", ");
+     Serial.println(b); 
+     if(a <= 0 || b <= 0 || a > 170 || b > 170){
+      freeze();
+      delay(2000);
+    //  foo = 0;
+      return 0;
+     }
+     if(digitalRead(dir1) || digitalRead(dir2)){
+        return ((a - setPoint) + (a - b)/5);
+     }
+    else{
+        return ((b - setPoint) + (b - a)/5);
+      }
+}
+
+void manualLoco(){
+    analogWrite(pwm1, turnSpeed);
+    analogWrite(pwm2, turnSpeed);
+    analogWrite(pwm3, turnSpeed);
+    analogWrite(pwm4, turnSpeed);
+  }
+void accelerate(){
+        analogWrite(pwm1, currentSpeed);
+        analogWrite(pwm2, currentSpeed);
+        analogWrite(pwm3, currentSpeed);
+        analogWrite(pwm4, currentSpeed);
+        delay(4);
+        currentSpeed++;
+        Serial.println(currentSpeed);             
+  }
+void freeze(){
+    for( ; currentSpeed > 0; currentSpeed--){
+        analogWrite(pwm1, currentSpeed);
+        analogWrite(pwm2, currentSpeed);
+        analogWrite(pwm3, currentSpeed);
+        analogWrite(pwm4, currentSpeed);
+        delay(5);
+       // Serial.println(currentSpeed);
+      }
+      foo = 0;
+      bar = 0;
+      analogWrite(pwm1, 0);
+      analogWrite(pwm2, 0);
+      analogWrite(pwm3, 0);
+      analogWrite(pwm4, 0);
+  }
+
+
+void shoot(int r){
+/*  if(!toggle){
+    digitalWrite(r, HIGH);
+    toggle = true; 
+  }
+  else{
+    digitalWrite(r, LOW);
+    toggle = false;
+  }
+  Serial.println(digitalRead(r)); */
+  digitalWrite(r, HIGH);
+  delay(500);
+  digitalWrite(r, LOW);
+  Serial.println("shoot");
+  }
---
+++ Top_final.ino
@@ -0,0 +1,250 @@
+#include <TM1637Display.h>
+#include <Arduino.h>
+//#include <EEPROM.h>
+/*SFIT ROBOCON TEAM
+ *CONFIDENTIAL
+ *18/02/2017
+ *promise day
+ *Locomotion Code
+ *Straight Outta Programmer's Desk
+ *Theme: Asobi(The flying disc)
+*/
+// motor 1 is for angle
+//motor 2 and motor 3 is for shooting
+
+#include<Wire.h>
+
+void action(char x);
+void receive(void);
+void setAngle(int);
+
+#define CLK1 51
+#define DIO1 53
+#define CLK2 47
+#define DIO2 49
+#define m1p1 26
+#define m1p2 24
+#define m2p1 28
+#define m2p2 30
+#define m3p1 32
+#define m3p2 34
+#define pwm1 12
+#define pwm2 11
+#define pwm3 10
+#define STEP 100
+//#define RELAY1 37
+//#define RELAY2 35
+
+TM1637Display displayAngle= TM1637Display(CLK1, DIO1);
+TM1637Display displaySpeed= TM1637Display(CLK2, DIO2);
+
+int MAX = 200;
+int MIN = 24;
+int defaultSpeed = 24;
+//int relayPin = RELAY1;
+int encoderPin1 = 2;
+int encoderPin2 = 3;
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+long lastencoderValue = 0; 
+int lastMSB = 0;
+int lastLSB = 0;
+long int bar = 0;
+int dir = 0;
+//bool side = false;
+//int addr = 0; // EPROM address to store encoder value
+
+void updateEncoder(void);
+
+void setup() {
+ // EEPROM.write(0,0);
+ // EEPROM.write(1,0);
+  pinMode(m1p1, OUTPUT);
+  pinMode(m1p2, OUTPUT);
+  pinMode(pwm1, OUTPUT);
+  digitalWrite(m1p1, LOW);
+  digitalWrite(m1p2, LOW);
+  analogWrite(pwm1, 50);
+  pinMode(m2p1, OUTPUT);
+  pinMode(m2p2, OUTPUT);
+  pinMode(pwm2, OUTPUT);
+  digitalWrite(m2p1, HIGH);
+  digitalWrite(m2p2, LOW);
+  analogWrite(pwm2, 0);
+
+  pinMode(m3p1, OUTPUT);
+  pinMode(m3p2, OUTPUT);
+  pinMode(pwm3, OUTPUT);
+  digitalWrite(m3p1, HIGH);
+  digitalWrite(m3p2, LOW);
+  analogWrite(pwm3, 0);
+//  pinMode(RELAY1, OUTPUT);
+//  pinMode(RELAY2, OUTPUT);
+  
+  Wire.begin(8);
+  Wire.onReceive(receive);
+
+  pinMode(encoderPin1, INPUT); 
+  pinMode(encoderPin2, INPUT);
+ 
+  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+ 
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3) 
+  attachInterrupt(digitalPinToInterrupt(2), updateEncoder, CHANGE); 
+  attachInterrupt(digitalPinToInterrupt(3), updateEncoder, CHANGE);
+  //if(EEPROM.read(1) == 1)
+  //  encoderValue = -EEPROM.read(addr)*100;
+  //else
+  //  encoderValue = EEPROM.read(addr)*100;
+  //bar = encoderValue;
+  Serial.begin(9600);
+//  Serial.println(EEPROM.read(addr));
+ // Serial.println(bar);
+  //7 segment functions
+  displaySpeed.setBrightness(20);
+  displayAngle.setBrightness(20);
+  displaySpeed.showNumberDec(0);
+  displayAngle.showNumberDec(encoderValue);
+  
+}
+
+void loop() {
+    displayAngle.showNumberDec(abs(encoderValue)/100);
+   // Serial.println("I'm ON");
+    if(dir == 1){
+       while(encoderValue > (bar - STEP)){
+        //  Serial.println(encoderValue);
+            displayAngle.showNumberDec(abs(encoderValue)/100);
+        }
+        digitalWrite(m1p1, LOW);
+        digitalWrite(m1p2, LOW);
+        dir = 0;
+    //    EEPROM.write(addr, abs(encoderValue)/100);
+    //    if(encoderValue < 0){
+    //        EEPROM.write(1, 1);
+    //      }
+    //    else
+    //        EEPROM.write(1, 0);
+      }
+     if(dir == 2){
+        while(encoderValue < (bar + STEP)){
+        //   Serial.println(encoderValue); 
+            displayAngle.showNumberDec(abs(encoderValue)/100); 
+        }
+        digitalWrite(m1p1, LOW);
+        digitalWrite(m1p2, LOW);
+        dir = 0;
+     //   EEPROM.write(addr, abs(encoderValue)/100);
+     //   if(encoderValue < 0)
+     //       EEPROM.write(1, 1);
+     //   else
+     //       EEPROM.write(1, 0);
+      }
+     
+        
+}
+
+void receive(){  
+ // while(1 < Wire.available()){
+ //   char c = Wire.read();
+ //   Serial.println(c);
+ //   }
+  char foo = Wire.read();
+ // Serial.println(foo);
+  action(foo);  
+}
+
+void action(char foo){
+  
+  if(foo == '+'){
+    defaultSpeed += 3;
+    if(defaultSpeed > MAX){
+        defaultSpeed = MAX;
+      }
+    analogWrite(pwm2, defaultSpeed);
+    analogWrite(pwm3, defaultSpeed);
+   // Serial.println(defaultSpeed);
+    displaySpeed.showNumberDec(defaultSpeed);
+   // Serial.println("inside +");
+    
+  }
+  
+  else if(foo == '-'){
+    defaultSpeed -= 3;
+    if(defaultSpeed < MIN){
+      defaultSpeed = MIN;
+      analogWrite(pwm2, 0);
+      analogWrite(pwm3, 0);
+     // Serial.println(defaultSpeed);
+      displaySpeed.showNumberDec(0);
+      return;
+      }
+    analogWrite(pwm2, defaultSpeed);
+    analogWrite(pwm3, defaultSpeed);
+   // Serial.println(defaultSpeed);
+    displaySpeed.showNumberDec(defaultSpeed); 
+   // Serial.println("inside -");   
+  }
+
+  else if(foo == 'L'){
+      digitalWrite(m2p1, LOW);
+      digitalWrite(m2p2, HIGH);
+      digitalWrite(m3p1, LOW);
+      digitalWrite(m3p2, HIGH);
+//      side = true;
+//      Serial.println("DIR1");
+    }
+  else if(foo == 'R'){
+      digitalWrite(m2p1, HIGH);
+      digitalWrite(m2p2, LOW);
+      digitalWrite(m3p1, HIGH);
+      digitalWrite(m3p2, LOW);
+//      side = false;
+//      Serial.println("DIR2");    
+    }
+  else if(foo == 'U'){
+      digitalWrite(m1p1, HIGH);
+      digitalWrite(m1p2, LOW);
+      bar = encoderValue;
+      dir = 1;
+    }
+  else if(foo == 'D'){
+      digitalWrite(m1p1, LOW);
+      digitalWrite(m1p2, HIGH);
+      bar = encoderValue;
+      dir = 2;
+  }
+/*  else if(foo == 'S'){
+    if(!side){
+      relayPin = RELAY2;
+      }
+    else{ 
+      relayPin = RELAY1;
+      }
+    digitalWrite(relayPin, HIGH);
+    Serial.println(relayPin);
+  }
+  else if(foo == 'X'){
+    digitalWrite(relayPin, LOW);
+    Serial.println("off");
+    } */
+  else
+   // Serial.println("Error bro");
+}
+  
+void updateEncoder(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+ 
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number
+  int sum  = (lastEncoded << 2) | encoded; //adding it to the previous encoded value
+ 
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue++;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue--;
+ 
+  lastEncoded = encoded; //store this value for next time
+ // Serial.println(encoderValue);
+}
+  
